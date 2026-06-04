f691b5f8cb4269c13a589f89278c04e8e6bfcf16
slash128 <jg@granatlabs.com>
2026-06-04 12:42:54 -0700

    Final PCB updates

--- Software/DC34_Cnet_Badge/__vm/.DC34_Cnet_Badge.vsarduino.h
+++ Software/DC34_Cnet_Badge/__vm/.DC34_Cnet_Badge.vsarduino.h
@@ -5,23 +5,29 @@
 			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Yún                                                                                                                        (yun), Platform=avr, Package=arduino
+	Hardware: ESP32 Dev Module                                                                                                              (esp32_esp32), Platform=esp32, Package=esp32
 */
 
-#define __AVR_ATmega32u4__
-#define __AVR_ATmega32U4__
+#define __2601_ESp32__
+#define __2601_ESP32__
 #define ARDUINO 108010
 #define ARDUINO_MAIN
-#define F_CPU 16000000L
-#define __AVR__
-#define F_CPU 16000000L
+#define F_CPU 240000000L
+#define printf iprintf
+#define __ESP32__
+#define F_CPU 240000000L
 #define ARDUINO 108010
-#define ARDUINO_AVR_YUN
-#define ARDUINO_ARCH_AVR
-#define USB_VID 0x2341
-#define USB_PID 0x8041
-#define USB_MANUFACTURER "\"Unknown\""
-#define USB_PRODUCT "\"Arduino
+#define ARDUINO_ESP32_DEV
+#define ARDUINO_ARCH_ESP32
+#define ARDUINO_BOARD "ESP32_DEV"
+#define ARDUINO_VARIANT "esp32"
+#define ARDUINO_PARTITION_default
+#define ARDUINO_HOST_OS "windows"
+#define ARDUINO_FQBN "{build.fqbn}"
+#define CORE_DEBUG_LEVEL 0
+#define ARDUINO_RUNNING_CORE 1
+#define ARDUINO_EVENT_RUNNING_CORE 1
+#define ARDUINO_USB_CDC_ON_BOOT 0
 #include "pins_arduino.h" 
 #include "arduino.h"
 #include "..\DC34_Cnet_Badge.ino"
